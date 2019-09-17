d64b701b3f34ee34ead282ae00d55539a2b4f101
zengyimin <yimin.zeng@igg.com>
2019-09-17 17:21:46 +0800

    add check seq to download

--- cmd/http/httpServer.go
+++ cmd/http/httpServer.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // hello world, the web server
@@ -20,10 +24,50 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, `{"code": 0, "data":0 }`)
 }
 
+type EtcdPut struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+	Lease string `json:"lease,omitempty"`
+}
+
+var count = 0
+
+func etcdLoad(w http.ResponseWriter, req *http.Request) {
+	key := "/zengym/testload" + strconv.Itoa(count)
+	count++
+	value := "111111111111111111111111111111"
+	key = base64.StdEncoding.EncodeToString([]byte(key))
+	value = base64.StdEncoding.EncodeToString([]byte(value))
+	data := EtcdPut{key, value, ""}
+	b, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("send data", string(b))
+	resp, err := http.Post("http://10.14.3.229:23791/v3alpha/kv/put", "application/x-www-form-urlencoded", strings.NewReader(string(b)))
+	if err != nil {
+		fmt.Println("post error ", err)
+		w.WriteHeader(400)
+		io.WriteString(w, err.Error())
+	}
+	defer resp.Body.Close()
+	//io.Reader
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read response error ", err)
+		w.WriteHeader(500)
+		io.WriteString(w, err.Error())
+	}
+	w.Write(body)
+	fmt.Println(body)
+}
 func main() {
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/etcdload", etcdLoad)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	fmt.Println("server end")
 }
--- cmd/m3u8Downloader/main.go
+++ cmd/m3u8Downloader/main.go
@@ -17,104 +17,153 @@ import (
 )
 
 var allTss map[string]bool
-var atagRegExp = regexp.MustCompile(`<a[^>]+[(href)|(HREF)]\s*\t*\n*=\s*\t*\n*[(".+")|('.+')][^>]*>[^<]*</a>`) //以Must前缀的方法或函数都是必须保证一定能执行成功的,否则将引发一次panic
+var wg sync.WaitGroup
+var jobs chan string
+var faileds chan string
+var base string
+
 func getPlaylist(url string) string {
+	body, err := ioutil.ReadFile(url)
+	if err == nil {
+		return string(body)
+	}
+	m3u8name := url
+	l := strings.LastIndex(m3u8name, "/")
+	if l != -1 {
+		m3u8name = m3u8name[l+1:]
+	}
 	if strings.HasPrefix(url, "http") {
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Println(err)
+			log.Fatal(err)
 		}
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
-		err = ioutil.WriteFile("save.m3u8", body, 0666)
+		err = ioutil.WriteFile(m3u8name, body, 0666)
 		if err != nil {
 			fmt.Println(err)
-			return ""
-		}
-		return string(body)
-	} else {
-		body, err := ioutil.ReadFile(url)
-		if err != nil {
-			fmt.Println(err)
-			return ""
-		}
-		err = ioutil.WriteFile("save.m3u8", body, 0666)
-		if err != nil {
-			fmt.Println(err)
-			return ""
+			log.Fatal(err)
 		}
 		return string(body)
 	}
+	log.Fatal("no m3u8 content")
+	return ""
+}
+func downloadFile(file string, force bool) (int64, error) {
+	if !force {
+		info, err := os.Stat(file)
+		if err == nil && info.Size() > 1024 {
+			fmt.Printf("%s already download size:%d\n", file, info.Size())
+			return info.Size(), nil
+		}
+	}
+	url := base + "/" + file
+	var body []byte
+	fmt.Println("start download ", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	if body[0] == '<' {
+		return 0, fmt.Errorf("resp error")
+	}
+	err = ioutil.WriteFile(file, body, 0666)
+	if err != nil {
+		os.Remove(file)
+		fmt.Println(err)
+		return 0, err
+	}
+	fmt.Printf("download success %s size:%d\n", url, len(body))
+	return int64(len(body)), nil
 
 }
-
-var wg sync.WaitGroup
-
-func getTsFile(files chan string) error {
+func getTsFile() error {
 	wg.Add(1)
 	defer wg.Done()
 	var err error
-	for file := range files {
-
-		info, err := os.Stat(file)
-		if err == nil && info.Size() > 0 {
-			fmt.Printf("%s already download size:%d\n", file, info.Size())
-			continue
-		}
-		url := base + "/" + file
-		var body []byte
-		fmt.Println("start download ", url)
-		resp, err := http.Get(url)
+	for file := range jobs {
+		_, err := downloadFile(file, false)
 		if err != nil {
-			fmt.Println(err)
-			goto failed
+			fmt.Println("download failed", file)
+			faileds <- file
 		}
-		defer resp.Body.Close()
-		body, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-			goto failed
-		}
-		err = ioutil.WriteFile(file, body, 0666)
-		if err != nil {
-			fmt.Println(err)
-			goto failed
-		}
-		fmt.Println("download success ", url)
-		continue
-	failed:
-		fmt.Println("download failed", url)
-		files <- file
-
 	}
 	fmt.Println("download channel quit ", err)
 	return err
 }
-func downloadAllTs(m3u8 string) {
-	jobs := make(chan string, 10)
-	for w := 1; w <= 5; w++ {
-		go getTsFile(jobs)
+func dealFailed() {
+	for failedFile := range faileds {
+		fmt.Println("add failed download file ", failedFile)
+		jobs <- failedFile
 	}
+}
+func startDownloadWorker() {
+	jobs = make(chan string, 10)
+	faileds = make(chan string, 1000)
+	for w := 1; w <= 5; w++ {
+		go getTsFile()
+	}
+	go dealFailed()
+	wg.Wait()
+}
+func downloadAllTs(m3u8 string, checkSeq bool) {
+	startDownloadWorker()
 	allTss = make(map[string]bool)
 	scanner := bufio.NewScanner(strings.NewReader(m3u8))
+	var lastSeq = -1
 	for scanner.Scan() {
 		text := scanner.Text()
 		if strings.HasPrefix(text, "#EXTINF") {
-			text = text[0 : len(text)-1]
-			tmp := strings.Split(text, ":")
-			duration, _ := strconv.ParseFloat(tmp[1], 32)
+			reg := regexp.MustCompile(`#EXTINF:([\d]+)`)
+			strDuration := reg.FindStringSubmatch(text)
+			if strDuration == nil {
+				fmt.Println("regex can't find ts duration")
+			}
+			duration, err := strconv.ParseFloat(strDuration[1], 32)
+			if err != nil {
+				fmt.Println("duration error")
+			}
 			if scanner.Scan() {
 				filename := scanner.Text()
+				reg := regexp.MustCompile(`([\d]+)\.ts`)
+				thisSeq := reg.FindStringSubmatch(filename)
+				if thisSeq == nil {
+					fmt.Println("regex can't find ts file")
+				}
+				thisSeqNum, err := strconv.Atoi(thisSeq[1])
+				if err != nil {
+					fmt.Println(err)
+					break
+				}
+				if thisSeqNum != lastSeq+1 {
+					fmt.Printf("seq error %d-%d", lastSeq, thisSeqNum)
+					if checkSeq {
+						break
+					}
+				}
+				lastSeq = thisSeqNum
 				if v, ok := allTss[filename]; ok {
 					fmt.Println("find duplicate ts", v)
-					break
+					if checkSeq {
+						break
+					}
 				}
 				allTss[filename] = true
 				fmt.Println(duration, filename)
-				jobs <- filename
+				if !checkSeq {
+					jobs <- filename
+				}
+
 			}
 
 		}
@@ -124,16 +173,8 @@ func downloadAllTs(m3u8 string) {
 	if scanner.Err() != nil {
 		fmt.Printf(" > Failed!: %v\n", scanner.Err())
 	}
-	wg.Wait()
-}
 
-type Pkt struct {
-	media_type   string
-	pkt_pts      string
-	pkt_pts_time string
 }
-
-var pkts []Pkt
 
 func parsePkt(pkt string) (v map[string]string, err error) {
 	//fmt.Println("pkt ", pkt)
@@ -154,12 +195,37 @@ func parsePkt(pkt string) (v map[string]string, err error) {
 	return
 }
 
-// func parsePkt1(pkt string) (pts, ptsTime int) {
+/*,
+        {
+            "codec_type": "audio",
+            "stream_index": 0,
+            "pts": 877690890,
+            "pts_time": "9752.121000",
+            "dts": 877690890,
+            "dts_time": "9752.121000",
+            "duration": 2089,
+            "duration_time": "0.023211",
+            "size": "457",
+            "pos": "3572",
+            "flags": "K_",
+            "side_data_list": [
+                {
+                    "side_data_type": "MPEGTS Stream ID"
+                }
+            ]
+		}
+*/
+func getLastPkt(filename string) string {
 
-// }
-func getLastPkt(base string, filename string) string {
+	info, err := os.Stat(filename)
+	if err != nil || info.Size() < 1024 {
+		_, err := downloadFile(filename, true)
+		if err != nil {
+			return ""
+		}
+	}
 	cmdProbeTs := "ffprobe -show_packets -of json "
-	cmdProbeTs += base + "/" + filename
+	cmdProbeTs += "." + "/" + filename
 	list := strings.Split(cmdProbeTs, " ")
 	cmd := exec.Command(list[0], list[1:]...)
 	// stderrOutPut,err := cmd.StderrPipe()
@@ -168,101 +234,140 @@ func getLastPkt(base string, filename string) string {
 	// }
 	pwdOutput, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println(err)
+		if strings.Contains(base, "http") {
+			fmt.Println("ffprobe error restart download", filename)
+			_, err := downloadFile(filename, true)
+			if err != nil {
+				log.Fatal(err)
+				return ""
+			}
+			return getLastPkt(filename)
+		}
+		return ""
 	}
 	//fmt.Println(string(pwdOutput))
-	lastPkt := string(pwdOutput[bytes.LastIndex(pwdOutput, []byte("{")):])
+	lastPkt := string(pwdOutput[bytes.LastIndex(pwdOutput, []byte("codec_type")):])
 	return lastPkt
 }
-func calcM3u8(file string) {
-	l := strings.LastIndex(file, "/")
-	base := "."
-	if l != -1 {
-		base = file[:l]
-	}
-	h, err := os.Open(file)
-	if err != nil {
-		fmt.Println(err)
-	}
+func calcM3u8(m3u8 string) {
 	totalDuration := 0.0
 	var lastPtsTime float64
-	scanner := bufio.NewScanner(h)
+	var lastSeq = -1
+	var err error
+	scanner := bufio.NewScanner(strings.NewReader(m3u8))
 	for scanner.Scan() {
 		//fmt.Println(scanner.Text())
 		text := scanner.Text()
 		if strings.HasPrefix(text, "#EXTINF") {
-			text = text[0 : len(text)-1]
-			tmp := strings.Split(text, ":")
-			strDuration := tmp[1]
-			l = strings.LastIndex(strDuration, ",")
-
-			if l != -1 {
-				strDuration = tmp[1][0:l]
+			//#EXTINF:2.000,
+			//1566196545993-129.ts
+			reg := regexp.MustCompile(`#EXTINF:([\d]+)`)
+			strDuration := reg.FindStringSubmatch(text)
+			if strDuration == nil {
+				fmt.Println("regex can't find ts duration")
+				break
 			}
-			duration, _ := strconv.ParseFloat(strDuration, 32)
+			duration, err := strconv.ParseFloat(strDuration[1], 32)
+			if err != nil {
+				fmt.Println("duration error")
+				break
+			}
 			totalDuration += duration
 			//parse file to get file duration
 			if scanner.Scan() {
 				filename := scanner.Text()
-				lastPkt := getLastPkt(base, filename)
+				reg := regexp.MustCompile(`([\d]+)\.ts`)
+				thisSeq := reg.FindStringSubmatch(filename)
+				if thisSeq == nil {
+					fmt.Println("regex can't find ts file")
+				}
+				thisSeqNum, err := strconv.Atoi(thisSeq[1])
+				if err != nil {
+					fmt.Println(err)
+					break
+				}
+				if thisSeqNum != lastSeq+1 {
+					fmt.Printf("seq error %d-%d", lastSeq, thisSeqNum)
+					break
+				}
+				lastSeq = thisSeqNum
+				lastPkt := getLastPkt(filename)
 				v, _ := parsePkt(lastPkt)
-				pts, _ := strconv.Atoi(v["pts"])
-				ptsTime, _ := strconv.ParseFloat(v["pts_time"], 32)
+				pts, err := strconv.Atoi(v["pts"])
+				if err != nil {
+					fmt.Println("no pts ")
+					break
+				}
+
+				ptsTime, err := strconv.ParseFloat(v["pts_time"], 32)
+				if err != nil {
+					fmt.Println("no pts_time ")
+					break
+				}
 				fileDuration := ptsTime - lastPtsTime
 				curDiff := fileDuration - duration
 				totalDiff := ptsTime - totalDuration
-				fmt.Println("list  ", filename, duration, totalDuration)
-				fmt.Println("file  ", pts, fileDuration, ptsTime)
-				fmt.Println("diff ", curDiff, totalDiff)
+				fmt.Printf("list  file:%s duration:%f,total:%f\n", filename, duration, totalDuration)
+				fmt.Printf("probe  duration:%f,pts:%d,ptsTime:%f \n", fileDuration, pts, ptsTime)
+				fmt.Printf("diff cur:%f,total:%f\n", curDiff, totalDiff)
 				if math.Abs(curDiff) > 1 || math.Abs(totalDiff) > 1 {
-					fmt.Errorf("curDiff %f totaldiff:%f", curDiff, totalDiff)
+					fmt.Printf("curDiff %f totaldiff:%f", curDiff, totalDiff)
+					break
 				}
 				lastPtsTime = ptsTime
 			}
 
 		}
-		//fmt.Println(totalDuration)
-
+	}
+	if err != nil {
+		log.Fatal(err)
 	}
 	if scanner.Err() != nil {
 		fmt.Printf(" > Failed!: %v\n", scanner.Err())
 	}
 }
 
-var base string
-
 func main() {
-	// content := []byte(`
-	// # json fragment
-	// "id": "dbsuye23sd83d8dasf7",
-	// "name": "Larry",
-	// "birth_year": 2000
-	// `)
-	// p := regexp.MustCompile(`(?m)"(?P\w+)":\s+"?(?P[a-zA-Z0-9]+)"?`)
-	// var dst []byte
-	// tpl := []byte("$key=$value\n")
-	// for _, submatches := range p.FindAllSubmatchIndex(content, -1) {
-	// 	dst = p.Expand(dst, tpl, content, submatches)
-	// }
-	// pat := `(?:#EXTINF:)(([+-]?([0-9]*[.])?[0-9]+))`
-	// re, _ := regexp.Compile(pat)
-	// allTs := re.FindAllString(ret, -1)
-	// for _, ts := range allTs {
-	// 	fmt.Println(ts)
-	// }
-	//fmt.Println(string(dst))
 	if len(os.Args) < 3 {
-		log.Fatal("please input m3u8 file")
+		log.Fatalf("please input as %s -d m3u8 url 1/0(check seq) ", os.Args[0])
 	}
+	// base = `https://streamcraft-sim.akamaized.net/qa/live/WeLive-QA_21000210_1027_311975/vod/`
+	// lastPkt := getLastPkt("1566219192265-4902.ts")
+	// v, _ := parsePkt(lastPkt)
+	// pts, err := strconv.Atoi(v["pts"])
+	// if err != nil {
+	// 	fmt.Println("no pts ")
+	// }
 
+	// ptsTime, err := strconv.ParseFloat(v["pts_time"], 32)
+	// if err != nil {
+	// 	fmt.Println("no pts_time ")
+	// }
+	// fmt.Println(v, pts, ptsTime)
+	url := os.Args[2]
+	if len(os.Args) > 3 {
+		base = os.Args[3]
+	} else {
+		if strings.Contains(url, "http") {
+			base = url[:strings.LastIndex(url, "/")]
+		} else {
+			l := strings.LastIndex(url, "/")
+			base = "."
+			if l != -1 {
+				base = url[:l]
+			}
+		}
+	}
+	m3u8 := getPlaylist(url)
 	if strings.Compare(os.Args[1], "-c") == 0 {
-		calcM3u8(os.Args[2])
+		calcM3u8(m3u8)
 	} else if strings.Compare(os.Args[1], "-d") == 0 {
-
-		url := os.Args[2]
-		ret := getPlaylist(url)
-		base = url[:strings.LastIndex(url, "/")]
-		downloadAllTs(ret)
+		startDownloadWorker()
+		checkSeq := false
+		if len(os.Args) > 3 && os.Args[3] == "1" {
+			checkSeq = true
+		}
+		downloadAllTs(m3u8, checkSeq)
 	} else {
 		fmt.Printf("input as %s -c m3u8file -d m3u8 url\n", os.Args[0])
 	}
